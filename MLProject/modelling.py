b3142a66848de05d230d8fa2e2c20386d296f5d5
nnnsrn <nisrinaizza36@gmail.com>
2025-12-12 15:58:23 +0700

    Refactor model training and prediction code

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -24,24 +24,20 @@ def train_model(df):
         X, y, test_size=0.2, random_state=42
     )
 
-    mlflow.set_experiment("Eksperimen_Nisrina_Drugs_Simple")
-
     mlflow.autolog()
 
-    with mlflow.start_run():
-        print("Training Model Logistic Regression...")
-        
-        # Train model
-        model = LogisticRegression(max_iter=200)
-        model.fit(X_train, y_train)
+    print("Training Model Logistic Regression...")
+    
+    # Train model
+    model = LogisticRegression(max_iter=200)
+    model.fit(X_train, y_train)
 
-        # Predict
-        y_pred = model.predict(X_test)
+    # Predict
+    y_pred = model.predict(X_test)
 
-        # Metric manual 
-        acc = accuracy_score(y_test, y_pred)
-        print(f"Training Selesai. Accuracy: {acc}")
-
+    # Metric
+    acc = accuracy_score(y_test, y_pred)
+    print(f"Training Selesai. Accuracy: {acc}")
 
 if __name__ == "__main__":
     data_path = "train_clean.csv" 
