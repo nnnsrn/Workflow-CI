b2840c42f58f0182aa22730ffdd8e11eb9a87499
nnnsrn <nisrinaizza36@gmail.com>
2025-12-12 15:55:20 +0700

    Refactor data loading and model training process

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -6,7 +6,43 @@ import mlflow
 import mlflow.sklearn
 import os
 
-# Load dataset
+def load_data(path):
+    print(f"Loading data from {path}...")
+    df = pd.read_csv(path)
+    return df
+
+def train_model(df):
+    if 'Effectiveness' not in df.columns:
+        print("Error: Kolom 'Effectiveness' tidak ditemukan.")
+        return
+
+    X = df.drop("Effectiveness", axis=1)
+    y = df["Effectiveness"]
+
+    # Split dataset
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
+    )
+
+    mlflow.set_experiment("Eksperimen_Nisrina_Drugs_Simple")
+
+    mlflow.autolog()
+
+    with mlflow.start_run():
+        print("Training Model Logistic Regression...")
+        
+        # Train model
+        model = LogisticRegression(max_iter=200)
+        model.fit(X_train, y_train)
+
+        # Predict
+        y_pred = model.predict(X_test)
+
+        # Metric manual 
+        acc = accuracy_score(y_test, y_pred)
+        print(f"Training Selesai. Accuracy: {acc}")
+
+
 if __name__ == "__main__":
     data_path = "train_clean.csv" 
     
@@ -15,29 +51,3 @@ if __name__ == "__main__":
         train_model(df)
     else:
         print(f"Error: File tidak ditemukan di {data_path}")
-
-X = df.drop("Deposit", axis=1)
-y = df["Deposit"]
-
-# Split dataset
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42
-)
-
-# MLflow autolog
-mlflow.autolog()
-
-with mlflow.start_run():
-
-    # Train model
-    model = LogisticRegression(max_iter=200)
-    model.fit(X_train, y_train)
-
-    # Predict
-    y_pred = model.predict(X_test)
-
-    # Metric
-    acc = accuracy_score(y_test, y_pred)
-    print("Accuracy:", acc)
-
-print("Training selesai. Cek MLflow UI.")
